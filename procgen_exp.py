d146f49dae802474cbbf9675421069a1ca010c3f
Shizhe Cai <caishizhe@outlook.com>
2020-08-20 19:31:18 +1000

    finish plot

--- procgen_exp.py
+++ procgen_exp.py
@@ -1,26 +1,49 @@
-import pickle
 from collections import OrderedDict
+import matplotlib.pyplot as plt
+import numpy as np
+
 
 def plot_normalized_mean_return(Rmax, Rmin, env):
-    output = [tuple()]
+    output_dict = load_slurm_output(env)
 
-    last_tuple = tuple()
-    while True:
-        try:
-            obj = pickle.load(open('data/'+env+'.pkl', 'rb'))
-            print(obj)
-            frames = obj.get("frames")
-            exp = lambda x: x if x is not "nan" else 0
-            episode_mean_return = exp(obj.get("mean_episode_return"))
-            current_tuple = (frames, episode_mean_return)
-            if not current_tuple == last_tuple:
-                output.append(current_tuple)
-            last_tuple = current_tuple
-        except EOFError:
-            break
+    frames = np.array(list(output_dict.keys()))
+    mean_episode_return = np.array([x.get("mean_episode_return", 0.0) for x in output_dict.values()])
 
-    print(output)
+    normalized_mean_return = (mean_episode_return - Rmin) / (Rmax - Rmin)
+
+    plt.plot(frames, mean_episode_return)
+    plt.title("frames vs mean_episode_return")
+    plt.show()
+
+    plt.plot(frames, normalized_mean_return)
+    plt.title("frames vs normalized mean return")
+
+    plt.show()
+
+
 
 def load_slurm_output(env_name):
-    pass
+    output_dict = OrderedDict()
+    with open("data/"+env_name+".out", "r") as file:
+        for line in file:
+            if line.startswith('[') and "frames" in line:
+                frames = [int(s) for s in line.split() if s.isdigit()][0]
+            else:
+                line = line.strip("\n")
+                line = line.replace("nan", "0.0")
+                if line.startswith('{'):
+                    lines = line
+                    if '}' in line:
+                        stat_dict = eval(lines)
+                        output_dict[frames] = stat_dict
+                elif line.endswith("}"):
+                    lines += line
+                    stat_dict = eval(lines)
+                    output_dict[frames] = stat_dict
+                else:
+                    lines += line
 
+    return output_dict
+
+
+plot_normalized_mean_return(10, 5, "coinrun")
